a949ca6e64cdb4eeb3fd53aad266e5007d82a668
renan <renan.rgsilva@gmail.com>
2017-12-08 09:54:08 -0200

    Test Case for DAO

---
+++ parser-log/src/test/java/com/ef/test/dao/TestDAO.java
@@ -0,0 +1,14 @@
+package com.ef.test.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDAO {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
